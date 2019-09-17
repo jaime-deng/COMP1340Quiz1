07c973079684c10d4ad7273d9af1aec5b2e63793
Jaime Deng <jaime@jaimes-mbp.wifi.bcit.ca>
2019-09-17 14:21:57 -0700

    First commit

---
+++ Question1/q1.js
@@ -0,0 +1,3 @@
+/* Git is different from github in that git is a tool that helps a user create save points in their work which can be reverted back to; meanwhile, github is a code sharing service that allows us to share code.
+
+Git is a tool to create checkpoints whereas github is a service to share code. */
